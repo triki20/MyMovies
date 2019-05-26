d3ab24f3aa973acdae8fb2a3932d4888a8aa5012
yahalom <triki20@gmail.com>
2019-05-26 14:34:41 +0300

    Initial Director Routes

---
+++ routes/directors.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const router = express.Router();
+const Director = require('../models/director');
+
+// All directors routes
+router.get('/', async (req , res) => {
+    let searchOptions = {};
+    if(req.query.name != null && req.query.name !== ''){
+        searchOptions.name = new RegExp(req.query.name, 'i')
+    }
+    try{
+        const directors = await Director.find(searchOptions);
+        res.render('directors/index' , {
+            directors,
+            searchOptions: req.query
+        });
+    }catch{
+        res.redirect('/');
+    }
+});
+
+// New directors routes
+router.get('/new', (req , res) => {
+    res.render('directors/new', { director: new Director() }); // director == _form_fields
+});
+
+// Create directors routes
+router.post('/', async (req , res) => {
+    const director = new Director({
+        name: req.body.name  //directorName
+    })
+    try{
+        const newDirector = await director.save()
+        //res.redirect(`directors/${director.id}`);
+        res.redirect('directors');
+    }catch{
+        res.render('directors/new', {
+            director,
+            errorMessage : 'Error creating Director'
+        });
+    }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -6,9 +6,11 @@ const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 // Routers connection
 const indexRouter = require('./routes/index');
+const directorsRouter = require('./routes/directors');
 
 const port = process.env.PORT || 3000; 
 
@@ -18,6 +20,7 @@ app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }));
 
 // Connection to the DB
 mongoose.connect(process.env.DATABASE_URL,{
@@ -28,6 +31,7 @@ db.on('error', error => console.error(error));
 db.once('open', () => console.log('Connected to Mongoose'));
 
 app.use('/', indexRouter);
+app.use('/directors', directorsRouter);
 
 app.listen(port, () => {
     console.log(`app listening on port ${port}`);
