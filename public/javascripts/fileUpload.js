310da755300a4a90317dfb137366e83c9cca582f
yahalom <triki20@gmail.com>
2019-06-04 13:41:39 +0300

    Finish Project

--- public/javascripts/fileUpload.js
+++ public/javascripts/fileUpload.js
@@ -1,13 +1,30 @@
-FilePond.registerPlugin(
-    FilePondPluginImagePreview,
-    FilePondPluginImageResize,
-    FilePondPluginFileEncode
-)
+const rootStyles = window.getComputedStyle(document.documentElement);
 
-FilePond.setOptions({
-    stylePanelAspectRatio: 150 / 100,
-    imageResizeTargetWidth: 100,
-    imageResizeTargetHeight: 150
-});
+if(rootStyles.getPropertyValue('--movie-cover-width-large') != null &&
+    rootStyles.getPropertyValue('--movie-cover-width-large') !== ''){
+     ready();
+}else{
+    document.getElementById('main-css').addEventListener('load', ready)
+}
 
-FilePond.parse(document.body)
+function ready(){
+    const coverWidth = parseFloat(rootStyles.getPropertyValue('--movie-cover-width-large'));
+    const coverAspectRatio = parseFloat(rootStyles.getPropertyValue('--movie-cover-aspect-ratio'));
+    const coverHeight = coverWidth / coverAspectRatio;
+
+    FilePond.registerPlugin(
+        FilePondPluginImagePreview,
+        FilePondPluginImageResize,
+        FilePondPluginFileEncode
+    )
+    
+    FilePond.setOptions({
+        stylePanelAspectRatio: 1 / coverAspectRatio,
+        imageResizeTargetWidth: coverWidth,
+        imageResizeTargetHeight: coverHeight
+    });
+    
+    FilePond.parse(document.body)
+}
+
+
